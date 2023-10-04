4349b363b5ccdf351b10683c84a6290cd43606a8
Olga Tananova <tananovao@yandex.ru>
2023-10-04 14:12:24 -0700

    second commit

---
+++ decomposingURL/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace decomposingurls
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // MARK: Setup
+            string exampleURL = "www.docs.microsoft.com/dotnet/csharp/whats-new/csharp-version-history";
+            // string exampleURL = "";
+            Console.WriteLine($"Hit ENTER to break down the URL into its component parts: {exampleURL}");
+            Console.ReadKey();
+
+            // MARK: Result
+            var components = BreakdownURL(exampleURL);
+            for (int i = 0; i < components.Length; i++)
+            {
+                var indent = new string(' ', i * 2);
+                Console.WriteLine($"{indent} -> {components[i]}");
+            }
+        }
+
+        // MARK: Calculation
+        public static string[] BreakdownURL(string urlString)
+        {
+            if (String.IsNullOrEmpty(urlString)) return Array.Empty<string>();
+            // string urlToParse = urlString.StartsWith("www.") ? urlString.Substring(4) : urlString;
+            //
+            // string[] array = urlToParse.Split('/');
+            //
+            // for (int i = 0; i < array.Length; i++)
+            // {
+            //     array[i] = array[i].Replace('-', ' ');
+            // }
+            //
+            // return array;
+            // Alternative 
+            string[] arrayOfStrings = urlString.Substring(4).Replace('-', ' ').Split('/');
+            return arrayOfStrings;
+        }
+    }
+}
---
+++ emailValidation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace validatingemails
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // MARK: Setup
+            Console.WriteLine("Enter the email address you'd like to validate:");
+            var input = Console.ReadLine();
+
+            // MARK: Result
+            ValidateEmail(input);
+            Console.ReadKey();
+        }
+
+        // MARK: Write your solution here
+        public static void ValidateEmail(string email)
+        {
+            if (email == "")
+            {
+                Console.WriteLine("You did not type anything.");
+                    return;
+            }
+            char firstChar = email[0];
+
+            if (!char.IsLetter(firstChar))
+            {
+                Console.WriteLine("Your email is not valid, it must not start with a symbol or a number.");
+                
+            } else if (!email.Contains('@'))
+            {
+                Console.WriteLine("Your email must contain '@' sign.");
+                
+            } else if (!email.EndsWith(".com"))
+            {
+                Console.WriteLine("Your must end with '.com'.");
+            }
+            else
+            {
+                Console.WriteLine("You entered valid email.");
+            }
+        }
+    }
+}
