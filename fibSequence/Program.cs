8816d26c486923d63bd53b631bea894a5b88548f
Olga Tananova <tananovao@yandex.ru>
2023-10-11 14:57:10 -0700

    fifth commit

---
+++ fibSequence/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace fibsequence
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // MARK: Setup
+            Console.WriteLine("Enter the number of Fibonacci elements you'd like to calculate:");
+            int input = int.Parse(Console.ReadLine());
+
+            // MARK: Result
+            var sequence = CalculateFibRecursion(input);
+            foreach (var fibonacci in sequence)
+            {
+                Console.WriteLine(fibonacci);
+                
+            }
+            Console.ReadKey();
+        }
+
+        // MARK: Write your solution here
+        public static List<int> CalculateFibonacci(int length)
+        {
+            List<int> sequence = new List<int>();
+            int firstMember = 0;
+            int secondMember = 1;
+            sequence.Add(firstMember);
+            sequence.Add(secondMember);
+            for (int i = 2; i < length; i++)
+            {
+                int nextMember = firstMember + secondMember;
+                sequence.Add(nextMember);
+                firstMember = secondMember;
+                secondMember = nextMember;
+            }
+            
+            return sequence;
+        }
+
+        public static List<int> CalculateFibRecursion(int length)
+        {
+            List<int> sequence = new List<int>();
+            int firstMember = 0;
+            int secondMember = 1;
+            sequence.Add(firstMember);
+            sequence.Add(secondMember);
+
+            void Hepler(int a, int b, List<int> array)
+            {
+                if (array.Count == length) return;
+                int nextMember = a + b;
+                array.Add(nextMember);
+                Hepler(b, nextMember, array );
+            }
+            Hepler(firstMember, secondMember, sequence);
+            return sequence;
+            
+        }
+    }
+}
---
+++ wordsOfAffirmation/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+
+namespace affirmingwords
+{
+    class MainClass
+    {
+        private static System.Timers.Timer timer;
+        static Random random = new Random();
+
+        static List<string> encouragements = new List<string>()
+        {
+            "Way to go!",
+            "Keep it up!",
+            "Almost there!",
+            "You're doing great!"
+        };
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            int randomEncouragement = random.Next(1, encouragements.Count);
+
+            Console.WriteLine(encouragements[randomEncouragement]);
+        }
+
+        public static void Main(string[] args)
+        {
+            // MARK: Setup
+
+            Console.WriteLine("Hit ENTER to start the timer!");
+            Console.ReadLine();
+
+            // MARK: Result
+            StartTimer(3000);
+
+            Console.WriteLine("You can end the timer anytime by pressing ENTER.\n");
+            Console.ReadLine();
+            StopTimer();
+        }
+
+        // MARK: Write your solution here...
+        public static void StartTimer(int interval)
+        {
+            timer = new System.Timers.Timer(interval);
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+
+        // 6
+        public static void StopTimer()
+        {
+            timer.Stop();
+            timer.Dispose();
+        }
+    }
+};
